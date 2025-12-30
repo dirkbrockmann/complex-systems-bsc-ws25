0cd78c772bbc861b278ff856dd599bec646b78f7
Dirk Brockmannn <brockmann.dirk@gmail.com>
2025-12-30 18:36:42 +0100

    Updates

---
+++ _astro/forEach.DRqkMsqZ.js
@@ -0,0 +1,1 @@
+import{i as e,b as r}from"./map.C5sRepdh.js";import{k as o}from"./toString.Ch9p6MpB.js";function u(n,i){for(var t=-1,f=n==null?0:n.length;++t<f&&i(n[t],t,n)!==!1;);return n}function c(n){return typeof n=="function"?n:e}function h(n,i){var t=o(n)?u:r;return t(n,c(i))}export{h as f};
